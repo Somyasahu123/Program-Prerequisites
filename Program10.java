63deb6de0c382dd802688618db476e3c7472d35d
Somyasahu123 <somyasahu221@gmail.com>
2025-01-09 00:14:58 +0530

    KilometersToMiles

---
+++ Program10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Program10{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		double kilometers = sc.nextDouble();
+		double miles = kilometers * 0.621371;
+		System.out.println(miles);
+	}
+}
