b04081c1799ec33c205afd817f4455f22ea4b42e
Somyasahu123 <somyasahu221@gmail.com>
2025-01-08 23:35:54 +0530

    Area

---
+++ Program4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Program4{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		double radius = sc.nextDouble();
+		double area = 3.14 * radius * radius;
+		System.out.println(area);
+	}
+}
